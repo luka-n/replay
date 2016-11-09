4ddb921cac1d291df8eb5ee2a04a94b8df228698
Luka Novsak <lnovsak@gmail.com>
2016-11-09 08:59:49 +0100

    proptypes with style

--- src/components/with-style.js
+++ src/components/with-style.js
@@ -3,6 +3,10 @@ import Radium from "radium";
 
 function withStyle(Component) {
   class ComponentWithStyle extends Component {
+    static propTypes = {
+      style: React.PropTypes.object
+    };
+
     render() {
       const style = [
         typeof(this.style) === "function" && this.style(),
