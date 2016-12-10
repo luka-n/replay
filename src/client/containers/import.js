a6c9f0b3492bb690724572b81be070bbb30d9cf7
Luka Novsak <lnovsak@gmail.com>
2016-12-10 22:02:02 +0100

    gpl all the things

--- src/client/containers/import.js
+++ src/client/containers/import.js
@@ -1,3 +1,20 @@
+// Copyright (C) 2016 Luka Novsak <lnovsak@gmail.com>
+//
+// This file is part of replay.
+//
+// replay is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// replay is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with replay.  If not, see <http://www.gnu.org/licenses/>.
+
 import {connect} from "react-redux";
 import Import from "../components/import";
 import * as actions from "../actions";
