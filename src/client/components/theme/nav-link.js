d8a7043176639acf80e71fbe6ff744ce6656ecbe
Luka Novsak <lnovsak@gmail.com>
2016-12-10 21:33:25 +0100

    active style for nav link

--- src/client/components/theme/nav-link.js
+++ src/client/components/theme/nav-link.js
@@ -5,9 +5,21 @@ import withStyle from "../with-style";
 const LinkWithStyle = withStyle(Link);
 
 class NavLink extends React.Component {
+  static propTypes = {
+    to: React.PropTypes.string.isRequired
+  };
+
+  static contextTypes = {
+    router: React.PropTypes.object.isRequired
+  };
+
+  isActive() {
+    return this.context.router.isActive(this.props.to, true);
+  }
+
   style() {
     return {
-      color: "#999",
+      color: this.isActive() ? "#fff" : "#999",
       cursor: "pointer",
       fontSize: "1.2rem",
       padding: "0rem 1rem",
