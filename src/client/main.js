6a2b23dec2f18a19bb6c765db70bf42fda11d72b
Luka Novsak <lnovsak@gmail.com>
2016-12-28 21:06:53 +0100

    don't use babel-node in production

--- src/client/main.js
+++ src/client/main.js
@@ -15,6 +15,8 @@
 // You should have received a copy of the GNU General Public License
 // along with replay.  If not, see <http://www.gnu.org/licenses/>.
 
+import "babel-polyfill";
+
 import React from "react";
 import ReactDOM from "react-dom";
 
